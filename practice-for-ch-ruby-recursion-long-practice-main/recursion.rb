cec1319d6426575f4cbada7a6f21a32d02598874
acheung-94 <acheung@ucdavis.edu>
2024-01-24 11:41:46 -0800

    finish fib, continue bsearch

--- practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
@@ -55,3 +55,64 @@ end
 # p exp_2(5, 3) #125
 # p exp_1(2, 200000)
 # p exp_2(2, 200000)
+
+
+class Array 
+# [1, 2, [3, 4]]
+# [3, 4]
+    def deep_dup
+        res = []
+        self.each do |ele|
+            if ele.is_a?(Array) 
+                res << ele.deep_dup
+            else
+                res << ele
+            end
+        end
+        res 
+    end
+end
+
+# p a = [1, 2, [3, 4]]
+# b = a.deep_dup
+# b[2] << "cat"
+# p a
+# p b
+
+def fibonacci_array(n)
+    res = []
+    return [] if n == 0
+    return [0] if n == 1
+    return [0,1] if n == 2
+
+    prev_array = fibonacci_array(n-1)
+    res += prev_array
+    res << prev_array[-1] + prev_array[-2]
+    res
+end
+
+# p fibonacci_array(3)
+# p fibonacci_array(5)
+# p fibonacci_array(0)
+# p fibonacci_array(1)
+# p fibonacci_array(30)
+
+def bsearch(array, target) # [1, 2, 3, 4, 5], 5
+
+    midpoint = array.length / 2 # mid = index 2
+
+    return midpoint if array[midpoint] == target #3 != 5
+
+    #bsearch [1,2], 2 (less than)
+    #bsearch [4,5], 5 (more than) => 1  midpoint = 1
+    #bsearch [4], 4 = > 0 mid = 0
+    midpoint + bsearch() if more than #add 1 per recursion if looking in upper half
+    
+end
+
+#return the index of target
+#assume array is already sorted..
+#base case is when the ele @ midpoint is equal to target
+#return value is half the length of array
+#length = 5 => midpoint = length / 2
+#length = 4 => midpoint = length / 2
