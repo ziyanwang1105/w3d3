67fe659a1029fb6d19c258c5b9d3bf7361a9e57a
Jerry Wang <ziyanwang1105@gmail.com>
2024-01-24 13:37:36 -0800

    finish bsearch and merge sort

--- practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
@@ -57,19 +57,19 @@ end
 # p exp_2(2, 200000)
 
 
-class Array 
+class Array
 # [1, 2, [3, 4]]
 # [3, 4]
     def deep_dup
         res = []
         self.each do |ele|
-            if ele.is_a?(Array) 
+            if ele.is_a?(Array)
                 res << ele.deep_dup
             else
                 res << ele
             end
         end
-        res 
+        res
     end
 end
 
@@ -103,11 +103,24 @@ def bsearch(array, target) # [1, 2, 3, 4, 5], 5
 
     return midpoint if array[midpoint] == target #3 != 5
 
+    return nil if array.length <=1 && array[0] != target
+
+    if array[midpoint] > target
+        return bsearch(array[0...midpoint], target)
+    else
+        sub_result = bsearch(array[midpoint+1..-1], target)
+        if sub_result.nil?
+            return nil
+        else
+            return sub_result + midpoint + 1
+        end
+    end
+
     #bsearch [1,2], 2 (less than)
     #bsearch [4,5], 5 (more than) => 1  midpoint = 1
     #bsearch [4], 4 = > 0 mid = 0
-    midpoint + bsearch() if more than #add 1 per recursion if looking in upper half
-    
+    #add 1 per recursion if looking in upper half
+
 end
 
 #return the index of target
@@ -116,3 +129,67 @@ end
 #return value is half the length of array
 #length = 5 => midpoint = length / 2
 #length = 4 => midpoint = length / 2
+
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+
+
+#merge([1],[2]) => [1,2]
+#merge has to iterate through both arrays and sort them to one
+#merge([1],[]) => [1]
+
+def merge(arr1, arr2)
+
+    res = []
+    idx1 = 0
+    idx2 = 0
+    while res.length < arr1.length + arr2.length
+        eval = arr1[idx1] <=> arr2[idx2]
+        case eval
+            when 1
+                res << arr2[idx2]
+                idx2 += 1
+            when -1
+                res << arr1[idx1]
+                idx1 += 1
+            when nil
+                if arr1[idx1].nil?
+                    res << arr2[idx2]
+                else
+                    res << arr1[idx1]
+                end
+                idx1 += 1
+                idx2 += 1
+            when 0
+                res << arr1[idx1] << arr2[idx2]
+                idx1 += 1
+                idx2 += 1
+        end
+    end
+    res
+end
+
+# p merge([1,2,3], [4,5]) #[1,2,3,4,5]
+# p merge([4,5,6], [1,2]) # [1,2,3,4,5]
+# p merge([],[1])
+# p merge([1],[])
+
+def merge_sort(array)
+
+    return array if array.length <= 1
+    res= []
+    midpoint = array.length / 2
+    sub1 = merge_sort(array[0...midpoint])
+    sub2 = merge_sort(array[midpoint..-1])
+    res += merge(sub1, sub2)
+    res
+
+end
+p a= (1..10).to_a.shuffle
+p a
+p merge_sort(a)
